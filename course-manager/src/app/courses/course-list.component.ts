4bffaecb99f1bc3783f9675dd90d70088abfbc00
Steffanie Graner <graner.steffanie@gmail.com>
2022-05-04 10:38:52 -0300

    Injeção de Dependência.

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -8,28 +9,9 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
   courses: Course[] = []
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '',
-        price: 99.99,
-        code: 'ABC-123',
-        duration: 120,
-        rating: 4.4,
-        releaseDate: 'May 1, 2015'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: '',
-        price: 45.99,
-        code: 'TRE-098',
-        duration: 80,
-        rating: 4.8,
-        releaseDate: 'July 10, 2018'
-      }
-    ]
+    this.courses = this.courseService.retrieveAll()
   }
 }
